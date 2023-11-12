9c706b704ded2c3107b6e5bfddc148d710936170
lan.chu <lan.chu@datx.com.vn>
2023-11-12 13:05:02 +0700

    update

--- rides-and-fares/src/main/java/org/apache/flink/training/exercises/ridesandfares/RidesAndFaresExercise.java
+++ rides-and-fares/src/main/java/org/apache/flink/training/exercises/ridesandfares/RidesAndFaresExercise.java
@@ -19,6 +19,13 @@
 package org.apache.flink.training.exercises.ridesandfares;
 
 import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.state.MapState;
+import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -33,6 +40,7 @@ import org.apache.flink.training.exercises.common.sources.TaxiFareGenerator;
 import org.apache.flink.training.exercises.common.sources.TaxiRideGenerator;
 import org.apache.flink.training.exercises.common.utils.MissingSolutionException;
 import org.apache.flink.util.Collector;
+import scala.Tuple2;
 
 /**
  * The Stateful Enrichment exercise from the Flink training.
@@ -41,77 +49,90 @@ import org.apache.flink.util.Collector;
  */
 public class RidesAndFaresExercise {
 
-    private final SourceFunction<TaxiRide> rideSource;
-    private final SourceFunction<TaxiFare> fareSource;
-    private final SinkFunction<RideAndFare> sink;
+  private final SourceFunction<TaxiRide> rideSource;
+  private final SourceFunction<TaxiFare> fareSource;
+  private final SinkFunction<RideAndFare> sink;
 
-    /** Creates a job using the sources and sink provided. */
-    public RidesAndFaresExercise(
-            SourceFunction<TaxiRide> rideSource,
-            SourceFunction<TaxiFare> fareSource,
-            SinkFunction<RideAndFare> sink) {
+  /**
+   * Creates a job using the sources and sink provided.
+   */
+  public RidesAndFaresExercise(
+      SourceFunction<TaxiRide> rideSource,
+      SourceFunction<TaxiFare> fareSource,
+      SinkFunction<RideAndFare> sink) {
 
-        this.rideSource = rideSource;
-        this.fareSource = fareSource;
-        this.sink = sink;
+    this.rideSource = rideSource;
+    this.fareSource = fareSource;
+    this.sink = sink;
+  }
+
+  /**
+   * Creates and executes the pipeline using the StreamExecutionEnvironment provided.
+   *
+   * @return {JobExecutionResult}
+   * @throws Exception which occurs during job execution.
+   */
+  public JobExecutionResult execute() throws Exception {
+
+    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+    // A stream of taxi ride START events, keyed by rideId.
+    DataStream<TaxiRide> rides =
+        env.addSource(rideSource).filter(ride -> ride.isStart).keyBy(ride -> ride.rideId);
+
+    // A stream of taxi fare events, also keyed by rideId.
+    DataStream<TaxiFare> fares = env.addSource(fareSource).keyBy(fare -> fare.rideId);
+
+    // Create the pipeline.
+    rides.connect(fares).flatMap(new EnrichmentFunction()).addSink(sink);
+
+    // Execute the pipeline and return the result.
+    return env.execute("Join Rides with Fares");
+  }
+
+  /**
+   * Main method.
+   *
+   * @throws Exception which occurs during job execution.
+   */
+  public static void main(String[] args) throws Exception {
+
+    RidesAndFaresExercise job =
+        new RidesAndFaresExercise(
+            new TaxiRideGenerator(),
+            new TaxiFareGenerator(),
+            new PrintSinkFunction<>());
+
+    job.execute();
+  }
+
+  public static class EnrichmentFunction
+      extends RichCoFlatMapFunction<TaxiRide, TaxiFare, RideAndFare> {
+
+    private transient ValueState<TaxiRide> rideState;
+    private transient ValueState<TaxiFare> fareState;
+
+
+    @Override
+    public void open(Configuration config) throws Exception {
+      ValueStateDescriptor<TaxiRide> rideDescriptor = new ValueStateDescriptor<>("rideState",
+          TypeInformation.of(new TypeHint<TaxiRide>() {
+          }));
+      ValueStateDescriptor<TaxiFare> fareDescriptor = new ValueStateDescriptor<>("fareState",
+          TypeInformation.of(new TypeHint<TaxiFare>() {
+          }));
+      rideState = getRuntimeContext().getState(rideDescriptor);
+      fareState = getRuntimeContext().getState(fareDescriptor);
     }
 
-    /**
-     * Creates and executes the pipeline using the StreamExecutionEnvironment provided.
-     *
-     * @throws Exception which occurs during job execution.
-     * @return {JobExecutionResult}
-     */
-    public JobExecutionResult execute() throws Exception {
-
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
-        // A stream of taxi ride START events, keyed by rideId.
-        DataStream<TaxiRide> rides =
-                env.addSource(rideSource).filter(ride -> ride.isStart).keyBy(ride -> ride.rideId);
-
-        // A stream of taxi fare events, also keyed by rideId.
-        DataStream<TaxiFare> fares = env.addSource(fareSource).keyBy(fare -> fare.rideId);
-
-        // Create the pipeline.
-        rides.connect(fares).flatMap(new EnrichmentFunction()).addSink(sink);
-
-        // Execute the pipeline and return the result.
-        return env.execute("Join Rides with Fares");
+    @Override
+    public void flatMap1(TaxiRide ride, Collector<RideAndFare> out) throws Exception {
+      if (fareState.value)
     }
 
-    /**
-     * Main method.
-     *
-     * @throws Exception which occurs during job execution.
-     */
-    public static void main(String[] args) throws Exception {
+    @Override
+    public void flatMap2(TaxiFare fare, Collector<RideAndFare> out) throws Exception {
 
-        RidesAndFaresExercise job =
-                new RidesAndFaresExercise(
-                        new TaxiRideGenerator(),
-                        new TaxiFareGenerator(),
-                        new PrintSinkFunction<>());
-
-        job.execute();
     }
-
-    public static class EnrichmentFunction
-            extends RichCoFlatMapFunction<TaxiRide, TaxiFare, RideAndFare> {
-
-        @Override
-        public void open(Configuration config) throws Exception {
-            throw new MissingSolutionException();
-        }
-
-        @Override
-        public void flatMap1(TaxiRide ride, Collector<RideAndFare> out) throws Exception {
-            throw new MissingSolutionException();
-        }
-
-        @Override
-        public void flatMap2(TaxiFare fare, Collector<RideAndFare> out) throws Exception {
-            throw new MissingSolutionException();
-        }
-    }
+  }
 }
